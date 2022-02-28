28114e5a4758dbdec248abd03c891e94ffa3a88c
minji-dan <aksen569@naver.com>
2022-02-28 15:41:46 +0900

    ㅔ

--- ft_sort.c
+++ ft_sort.c
@@ -2,6 +2,29 @@
 
 void ft_sort_three(t_info *info)
 {
+    int head;
+    int mid;
+    int tail;
+
+    head = info->st_a_head->data.num;
+    mid = info->st_a_head->next->data.num;
+    tail = info->st_a_tail->data.num;
+    if (tail > mid && tail > head && tail > mid)
+    {
+        sa(info, 1);
+        rra(info, 1);
+    }
+    else if (mid > tail && mid > head && head > tail)
+    {
+        sa(info, 1);
+        ra(info, 1);
+    }
+    else if (tail > mid && head > mid && tail > head)
+        ra(info, 1);
+    else if (tail > mid && head > mid && head > tail)
+        sa(info, 1);
+    else if (mid > tail && mid > head && tail > head)
+        rra(info, 1);
 }
 
 void ft_sort_big(t_info *info)
