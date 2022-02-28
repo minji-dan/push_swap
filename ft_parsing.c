4bc3bf67ad6d45e049ec21a019e016191e91bd51
minji-dan <aksen569@naver.com>
2022-02-28 16:17:50 +0900

    feat : parsing 함수 구현

---
+++ ft_parsing.c
@@ -0,0 +1,36 @@
+#include "push_swap.h"
+
+void print_error(int n)
+{
+    if (n == 1)
+    {
+        ft_putendl_fd("Input error", 2);
+        exit(1);
+    }
+}
+
+void ft_parsing(int argc, char **argv, t_info *info)
+{
+    char **tmp;
+    int i;
+    int j;
+    int num;
+
+    i = 1;
+    while (i < argc)
+    {
+        if ()
+            print_error(1);
+        tmp = ft_split(argv[i], ' ');
+        j = 0;
+        while (tmp[j])
+        {
+            num = ft_atoll(tmp[j]);
+            push_back(num, &info->st_a_tail, &info->st_a_head);
+            info->st_a_size++;
+            j++;
+        }
+        free_vec((void **)tmp);
+        i++;
+    }
+}
