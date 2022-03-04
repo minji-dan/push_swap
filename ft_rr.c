df4c07f9e113594947676227c0b1a2cfa8b8761f
minji-dan <aksen569@naver.com>
2022-03-04 14:11:40 +0900

    feat : rra rrb rrr 기능 구현

---
+++ ft_rr.c
@@ -0,0 +1,31 @@
+#include "push_swap.h"
+
+void rra(t_info *info, int flag)
+{
+    int n;
+
+    if (pop_front(&info->top_a, &info->bot_a, &n))
+    {
+        push_back(n, &info->top_a, &info->bot_a);
+        if (flag == 1)
+            ft_putendl_fd("rra", 1);
+    }
+}
+
+void rrb(t_info *info, int flag)
+{
+    if (pop_front(&info->top_b, &info->bot_b, &n))
+    {
+        push_back(n, &info->top_b, &info->bot_b);
+        if (flag == 1)
+            ft_putendl_fd("rrb", 1);
+    }
+}
+
+void rrr(t_info *info, int flag)
+{
+    rra(info, 0);
+    rrb(info, 0);
+    if (flag == 1)
+        ft_putendl_fd("rrr", 1);
+}
