ba7bab1b2fbcfc926dc4c82165fdc4802e2e02e8
minji-dan <aksen569@naver.com>
2022-03-04 16:43:49 +0900

    style : 폴더 이동

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "libft.h"
+
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
 
@@ -22,12 +24,19 @@ typedef struct s_deque
 typedef struct s_info
 {
     int st_a_size;
-    int st_b_bsize;
+    int st_b_size;
     t_deque *st_a_head;
     t_deque *st_b_head;
     t_deque *st_a_tail;
     t_deque *st_b_tail;
     int *arr;
 } t_info;
+
+/* libft */
+void ft_bzero(void *s, size_t n);
+void ft_putendl_fd(char *s, int fd);
+size_t ft_strlen(const char *src);
+void *ft_calloc(size_t count, size_t size);
+char **ft_split(char const *s, char c);
 
 #endif
